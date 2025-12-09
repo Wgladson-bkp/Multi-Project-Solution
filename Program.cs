98a9a4bd71fd735d74a21e8873c991a1379cd733
Wgladson <wgladson@protonmail.com>
2025-12-09 00:27:39 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Multi_Project_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
